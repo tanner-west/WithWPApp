216a55fc0aa68c943f846f8a39dd04c494b73d94
Tanner <west.tanner.j@gmail.com>
2020-12-03 08:43:26 -0500

    render post titles and content to App.js

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {
   SafeAreaView,
   StyleSheet,
@@ -7,18 +7,43 @@ import {
   Text,
   StatusBar,
 } from 'react-native';
+import axios from 'axios';
 
 export default function App() {
+  const [postData, setPostData] = useState(undefined);
+  useEffect(() => {
+    axios
+      .get('https://withwp.app/wp-json/wp/v2/posts')
+      .then((res) => setPostData(res.data))
+      .catch((err) => console.log(err));
+  }, []);
   return (
     <>
       <StatusBar barStyle="dark-content" />
       <SafeAreaView>
         <ScrollView
           contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}></ScrollView>
+          style={styles.scrollView}>
+          <Text style={styles.heading}>My Posts</Text>
+          {postData
+            ? postData.map((post) => (
+                <View key={post.id}>
+                  <Text>{post.title.rendered}</Text>
+                  <Text>{post.content.rendered}</Text>
+                </View>
+              ))
+            : null}
+        </ScrollView>
       </SafeAreaView>
     </>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  heading: {
+    fontWeight: 'bold',
+  },
+  scrollView: {
+    padding: 10,
+  },
+});
