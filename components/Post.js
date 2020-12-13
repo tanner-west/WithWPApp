0b9be86f72d946329835f42cf65e74faea2be36a
Tanner <west.tanner.j@gmail.com>
2020-12-13 14:19:20 -0500

    use react-native-render-html

--- components/Post.js
+++ components/Post.js
@@ -5,12 +5,6 @@ import HTML from 'react-native-render-html';
 export default function Post({post: {id, title, content, acf}}) {
   return (
     <View>
-      {acf.main_image ? (
-        <Image
-          style={{height: 300}}
-          source={{url: acf.main_image.sizes.large}}
-        />
-      ) : null}
       <Text style={styles.title}>{title.rendered}</Text>
       <HTML source={{html: content.rendered}} />
     </View>
