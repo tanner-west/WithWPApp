452ceb8d92c2421cb06f6eb861c59cb3e2b6b070
Tanner <west.tanner.j@gmail.com>
2020-12-17 08:34:43 -0500

    add html-entities

--- components/Post.js
+++ components/Post.js
@@ -1,11 +1,14 @@
 import React, {useState, useEffect} from 'react';
 import {StyleSheet, View, Text, Image} from 'react-native';
+import {AllHtmlEntities} from 'html-entities';
+const entities = new AllHtmlEntities();
+
 import HTML from 'react-native-render-html';
 
 export default function Post({post: {id, title, content, acf}}) {
   return (
     <View>
-      <Text style={styles.title}>{title.rendered}</Text>
+      <Text style={styles.title}>{entities.decode(title.rendered)}</Text>
       <HTML source={{html: content.rendered}} />
     </View>
   );
